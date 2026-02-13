f409a2b6518506ae9ff4b7c37b6fd1774886de2d
Jorge Olea <jorgeoleacasanova@gmail.com>
2026-02-13 12:32:27 +0100

    Initial commit - secure chat backend (clean slate)

---
+++ src/main/java/com/securechat/SecureChatServer.java
@@ -0,0 +1,17 @@
+package com.securechat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SecureChatServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecureChatServer.class);
+
+    public static void main (String[] args){
+
+        logger.info("Secure Chat Server starting...");
+
+        logger.info("Secure Chat Server started succesfully.");
+    }
+
+}
